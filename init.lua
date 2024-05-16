5ee6f0529c956cb5697f79e6a94c53f1b33b3632
shehanreshin <asp.shehanreshin@gmail.com>
2024-05-16 21:37:18 +0530

    Refactor treesitter to separate plugin file

--- init.lua
+++ init.lua
@@ -24,14 +24,6 @@ local opts = {}
 
 require("lazy").setup("plugins")
 
-local configs = require("nvim-treesitter.configs")
-
-      configs.setup({
-          ensure_installed = { "lua", "javascript", "html" },
-          highlight = { enable = true },
-          indent = { enable = true }  
-        })
-
 local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
