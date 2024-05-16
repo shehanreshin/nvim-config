fd9ce6baf013aeb919d23bf32a494b366ea23ded
shehanreshin <asp.shehanreshin@gmail.com>
2024-05-16 21:44:20 +0530

    Refactor telescope to separate plugin file

--- init.lua
+++ init.lua
@@ -24,8 +24,5 @@ local opts = {}
 
 require("lazy").setup("plugins")
 
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
 vim.keymap.set('n', '<C-g>', ':Neotree git_status<CR>', {})
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,9 +1,5 @@
 return {
 	{
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
-     dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-	{
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
     dependencies = {
