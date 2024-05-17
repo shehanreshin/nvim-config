675f71a7481d90df01942b8bc5386dca1dfbdf76
shehanreshin <asp.shehanreshin@gmail.com>
2024-05-17 09:55:52 +0530

    Add LSP configs for Lua and JS

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		local configs = require("nvim-treesitter.configs")
 		configs.setup({
-			ensure_installed = { "lua", "javascript", "html" },
+			ensure_installed = { "lua", "javascript", "html", "markdown" },
 			highlight = { enable = true },
 			indent = { enable = true }  
 		})
