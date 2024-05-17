0cdfd98a4a339363f63c15511292511d186b716f
shehanreshin <asp.shehanreshin@gmail.com>
2024-05-17 10:03:10 +0530

    Add some vim keymaps for lsp related functionality

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -20,6 +20,8 @@ return {
 			lspconfig.lua_ls.setup({})
 			lspconfig.tsserver.setup({})
 			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+			vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
 		end
 	}
 }
