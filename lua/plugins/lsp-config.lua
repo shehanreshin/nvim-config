5bed69a867766cf6fad88312555cdf680357d204
shehanreshin <asp.shehanreshin@gmail.com>
2024-05-18 17:38:07 +0530

    Add jdtls

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup {
- 		   ensure_installed = { "lua_ls", "tsserver" },
+ 		   ensure_installed = { "lua_ls", "tsserver", "jdtls" },
 			}
 		end
 	},
@@ -22,6 +22,9 @@ return {
 				capabilities = capabilities
 			})
 			lspconfig.tsserver.setup({
+				capabilities = capabilities
+			})
+			lspconfig.jdtls.setup({
 				capabilities = capabilities
 			})
 			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
