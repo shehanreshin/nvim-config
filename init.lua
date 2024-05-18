4d632f96d2e783bb9a5baaedb1f6c95f99b3130f
shehanreshin <asp.shehanreshin@gmail.com>
2024-05-18 18:39:50 +0530

    Enable line numbers

--- init.lua
+++ init.lua
@@ -1,22 +1,14 @@
-vim.cmd("set tabstop=2")
-vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
-
-vim.cmd(":map <Up> <Nop>")
-vim.cmd(":map <Left> <Nop>")
-vim.cmd(":map <Right> <Nop>")
-vim.cmd(":map <Down> <Nop>")
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,6 +1,6 @@
 vim.cmd("set tabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
+vim.cmd("set number")
 
 vim.cmd(":map <Up> <Nop>")
 vim.cmd(":map <Left> <Nop>")
