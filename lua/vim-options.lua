dfd339d877648d38534f77ff7e07901f6142d609
shehanreshin <asp.shehanreshin@gmail.com>
2024-05-18 21:10:35 +0530

    Update tab/indent space to 4

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,5 +1,5 @@
-vim.cmd("set tabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.cmd("set tabstop=4")
+vim.cmd("set shiftwidth=4")
 vim.cmd("set number")
 
 vim.cmd(":map <Up> <Nop>")
@@ -7,5 +7,5 @@ vim.cmd(":map <Left> <Nop>")
 vim.cmd(":map <Right> <Nop>")
 vim.cmd(":map <Down> <Nop>")
 
-vim.keymap.set('n', '<C-s>', ':w<CR>', {})
-vim.keymap.set('i', '<C-s>', '<C-o>:w<CR>', {})
+vim.keymap.set("n", "<C-s>", ":w<CR>", {})
+vim.keymap.set("i", "<C-s>", "<C-o>:w<CR>", {})
