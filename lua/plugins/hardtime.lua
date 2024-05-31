52077a79b05d0aea72ebd73ebea594d09bca0c7e
shehanreshin <asp.shehanreshin@gmail.com>
2024-05-31 09:44:39 +0530

    Update hardtime configuration to enable arrow keys in insert mode

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -1,8 +1,15 @@
 return {
 	"m4xshen/hardtime.nvim",
 	dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-	opts = {},
-	config = function()
-		require("hardtime").setup()
-	end,
+	opts = {
+		max_time = 1000,
+		max_count = 5,
+		hint = true,
+		disabled_keys = {
+			["<Up>"] = { "", "c", "t", "l" },
+			["<Down>"] = { "", "c", "t", "l" },
+			["<Left>"] = { "", "c", "t", "l" },
+			["<Right>"] = { "", "c", "t", "l" },
+		},
+	},
 }
