6a7f90cfe3db7960b83ec92d6be3383388c06111
shehanreshin <asp.shehanreshin@gmail.com>
2026-04-01 12:17:28 +0530

    Update colorscheme including that of dashboard

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,40 @@
+local ulquiorra = {
+  rosewater = "#DDF7EE",
+  flamingo = "#B7E9D7",
+  pink = "#9DE3D2",
+  mauve = "#8BA7C9",
+  red = "#E05B6F",
+  maroon = "#B94A5B",
+  peach = "#E59B6C",
+  yellow = "#D9C86F",
+  green = "#4FBE93",
+  teal = "#35B8AC",
+  sky = "#58C7D9",
+  sapphire = "#48AFCF",
+  blue = "#6C90D8",
+  lavender = "#A8B6E8",
+  text = "#EAF7F3",
+  subtext1 = "#BFD4CF",
+  subtext0 = "#94ABA8",
+  overlay2 = "#7A9190",
+  overlay1 = "#617776",
+  overlay0 = "#4A6161",
+  surface2 = "#364C4D",
+  surface1 = "#273A3D",
+  surface0 = "#1C2C31",
+  base = "#101A20",
+  mantle = "#0A1217",
+  crust = "#050A0D",
+}
+
 return {
   {
     "catppuccin/nvim",
     opts = {
       flavour = "mocha",
+      color_overrides = {
+        mocha = ulquiorra,
+      },
     },
   },
   {
--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -88,64 +88,85 @@ local right_header_width = max_width(split_lines(right_header))
 local quote = "If this eye cannot see a thing, then it does not exist"
 
 local palette = {
-  base = "#e0e0d4",
-  ink = "#0a0a0a",
-  shadow = "#282932",
-  green = "#2e8c6a",
-  deep_green = "#1a5d4d",
+  rosewater = "#DDF7EE",
+  flamingo = "#B7E9D7",
+  pink = "#9DE3D2",
+  mauve = "#8BA7C9",
+  red = "#E05B6F",
+  maroon = "#B94A5B",
+  peach = "#E59B6C",
+  yellow = "#D9C86F",
+  green = "#4FBE93",
+  teal = "#35B8AC",
+  sky = "#58C7D9",
+  sapphire = "#48AFCF",
+  blue = "#6C90D8",
+  lavender = "#A8B6E8",
+  text = "#EAF7F3",
+  subtext1 = "#BFD4CF",
+  subtext0 = "#94ABA8",
+  overlay2 = "#7A9190",
+  overlay1 = "#617776",
+  overlay0 = "#4A6161",
+  surface2 = "#364C4D",
+  surface1 = "#273A3D",
+  surface0 = "#1C2C31",
+  base = "#101A20",
+  mantle = "#0A1217",
+  crust = "#050A0D",
 }
 
 local function set_dashboard_highlights()
   vim.api.nvim_set_hl(0, "SnacksDashboardNormal", {
-    fg = palette.base,
-    bg = palette.shadow,
+    fg = palette.text,
+    bg = palette.base,
   })
   vim.api.nvim_set_hl(0, "SnacksDashboardBorder", {
-    fg = palette.shadow,
-    bg = palette.shadow,
+    fg = palette.surface1,
+    bg = palette.base,
   })
   vim.api.nvim_set_hl(0, "SnacksDashboardHeader", {
     fg = palette.green,
-    bg = palette.shadow,
+    bg = palette.base,
     bold = true,
   })
   vim.api.nvim_set_hl(0, "SnacksDashboardTitle", {
-    fg = palette.base,
-    bg = palette.shadow,
+    fg = palette.sky,
+    bg = palette.base,
     bold = true,
   })
   vim.api.nvim_set_hl(0, "SnacksDashboardIcon", {
-    fg = palette.green,
-    bg = palette.shadow,
+    fg = palette.teal,
+    bg = palette.base,
   })
   vim.api.nvim_set_hl(0, "SnacksDashboardKey", {
     fg = palette.green,
-    bg = palette.shadow,
+    bg = palette.base,
     bold = true,
   })
   vim.api.nvim_set_hl(0, "SnacksDashboardDesc", {
-    fg = palette.base,
-    bg = palette.shadow,
+    fg = palette.subtext1,
+    bg = palette.base,
   })
   vim.api.nvim_set_hl(0, "SnacksDashboardFooter", {
-    fg = palette.deep_green,
-    bg = palette.shadow,
+    fg = palette.sapphire,
+    bg = palette.base,
   })
   vim.api.nvim_set_hl(0, "SnacksDashboardFile", {
-    fg = palette.base,
-    bg = palette.shadow,
+    fg = palette.text,
+    bg = palette.base,
   })
   vim.api.nvim_set_hl(0, "SnacksDashboardDir", {
-    fg = palette.deep_green,
-    bg = palette.shadow,
+    fg = palette.blue,
+    bg = palette.base,
   })
   vim.api.nvim_set_hl(0, "SnacksDashboardSpecial", {
-    fg = palette.green,
-    bg = palette.shadow,
+    fg = palette.lavender,
+    bg = palette.base,
   })
   vim.api.nvim_set_hl(0, "SnacksDashboardQuote", {
-    fg = palette.deep_green,
-    bg = palette.shadow,
+    fg = palette.overlay2,
+    bg = palette.base,
     italic = true,
   })
 end
