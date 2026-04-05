4dfce6e31d850ec9fcd851a0fbcaac380d50518b
shehanreshin <asp.shehanreshin@gmail.com>
2026-04-05 12:59:01 +0530

    Add configurations to disable relative line numbering

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.lazyvim_eslint_auto_format = false
+vim.opt.number = true
+vim.opt.relativenumber = false
 
 vim.filetype.add({
   filename = {
